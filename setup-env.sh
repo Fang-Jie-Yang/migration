d18d97ccf8680f63dbc7c31e19b8fea9bb3f505b
b08902059 <b08902059@linux2.csie.ntu.edu.tw>
2023-02-19 14:40:40 +0800

    wip

--- setup-env.sh
+++ setup-env.sh
@@ -3,9 +3,8 @@
 username="fjyang"
 src_ip="10.10.1.1"
 dst_ip="10.10.1.2"
-qemu_branch="" #TODO
+qemu_branch="migration-demand-decrypt-v4.2.1-sw-multifd"
 sekvm_branch='demand-decrypt-v2'
-
 
 github_ssh_key="github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
 BGREEN='\033[1;32m'
@@ -72,7 +71,9 @@ EOF
 # $1: username
 # $2: ip
 function setupNetworkInterface() {
-    # TODO
+    ssh $1@$2 << EOF
+        sudo echo "@reboot /srv/vm/net.sh" | crontab -
+EOF
 }
 
 
--- setup-key.sh
+++ setup-key.sh
@@ -1,37 +1,18 @@
 #! /bin/bash
 
-if [[ $# -ne 2 ]]; then
-	echo "usage: ./setup-key.sh username client_ip"
-	exit
-fi
-
 BGREEN='\033[1;32m'
 BCYAN='\033[1;36m'
 BRED='\033[1;31m'
 NC='\033[0m'
 
 # generate ssh key for github/cloudlab
-if [[ ! -f m400-rsa.pub || ! -f m400-rsa ]]; then
+if [[ ! -f ~/.ssh/id_rsa.pub || ! -f ~/.ssh/id_rsa ]]; then
 
 	echo -e "${BCYAN}no key pair for m400, generating...${NC}"
-	ssh-keygen -f "./m400-rsa" 
-	echo -e "${BCYAN}please add m400-rsa.pub to your github account, and cloudlab key list.${NC}"
+	ssh-keygen
+	echo -e "${BCYAN}please add ~/.ssh/id_rsa.pub to your github account, and cloudlab key list.${NC}"
 	exit
 
 else
-	echo -e "${BGREEN}key pair exists, remember to add m400-rsa.pub to github and cloudlab${NC}"
+	echo -e "${BCYAN}key pair exists, remember to add ~/.ssh/id_rsa.pub to github and cloudlab${NC}"
 fi
-
-# upload key pair to m400(client)
-echo -e "${BCYAN}uploading key pair to m400${NC}"
-scp "m400-rsa" "$1@$2:~/.ssh/id_rsa"
-if [[ $? -ne 0 ]]; then
-	echo -e "${BRED}upload failed, exiting...${NC}"
-	exit
-fi
-scp "m400-rsa.pub" "$1@$2:~/.ssh/id_rsa.pub"
-if [[ $? -ne 0 ]]; then
-	echo -e "${BRED}upload failed, exiting...${NC}"
-	exit
-fi
-echo -e "${BGREEN}key pair uploaded${NC}"
