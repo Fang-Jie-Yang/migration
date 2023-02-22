692d295dcca95ef4ba0f7d7d3609436bf1b988f0
Tomato-059 <58678360+Fang-Jie-Yang@users.noreply.github.com>
2023-02-22 20:30:01 +0800

    Update setup-key.sh

--- setup-key.sh
+++ setup-key.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-
-
 BGREEN='\033[1;32m'
 BCYAN='\033[1;36m'
 BRED='\033[1;31m'
@@ -18,22 +16,3 @@ if [[ ! -f m400-rsa.pub || ! -f m400-rsa ]]; then
 else
 	echo -e "${BCYAN}key pair exists, remember to add m400-rsa.pub to github and cloudlab${NC}"
 fi
-
-if [[ $# -ne 2 ]]; then
-	echo "usage: ./setup-key.sh username client_ip"
-	exit
-fi
-
-# upload key pair to m400(client)
-echo -e "${BCYAN}uploading key pair to m400${NC}"
-scp "m400-rsa" "$1@$2:~/.ssh/id_rsa"
-if [[ $? -ne 0 ]]; then
-	echo -e "${BRED}upload failed, exiting...${NC}"
-	exit
-fi
-scp "m400-rsa.pub" "$1@$2:~/.ssh/id_rsa.pub"
-if [[ $? -ne 0 ]]; then
-	echo -e "${BRED}upload failed, exiting...${NC}"
-	exit
-fi
-echo -e "${BGREEN}key pair uploaded${NC}"
