daa8e067a013f0381cc3d736def57ef2a4b52fc1
Tomato-059 <58678360+Fang-Jie-Yang@users.noreply.github.com>
2023-03-04 20:17:51 +0800

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -228,7 +228,7 @@ for (( i = 0; i < $rounds; i++ )); do
     done
     echo -e "${BCYAN}setting migration capabilities${NC}" >&2
     for cap in ${CapsToSet[@]}; do
-        cmd="migrate_set_capability $cap ${CapSettings[$cap]}"
+        cmd="migrate_set_capability $cap ${CapsSettings[$cap]}"
         ncat -w 5 -i 2 $src_ip $src_monitor_port <<< "$cmd" 2>/dev/null >/dev/null
         ncat -w 5 -i 2 $dst_ip $dst_monitor_port <<< "$cmd" 2>/dev/null >/dev/null
     done
