ded797261e49154cf60579758443786b8ee6ab48
fjyang <fjyang@localhost>
2023-03-17 23:25:22 -0600

    also setup para-virt guest kernel too

--- setup-env.sh
+++ setup-env.sh
@@ -3,8 +3,9 @@
 username="fjyang"
 src_ip="10.10.1.1"
 dst_ip="10.10.1.2"
-qemu_branch="migration-demand-decrypt-v4.2.1-sw-multifd"
-sekvm_branch='demand-decrypt-v2'
+guest_branch="pv-v4.18-upcall"
+qemu_branch="migration-demand-decrypt-v4.2.1"
+sekvm_branch='v4.18-upcall'
 
 github_ssh_key="github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
 BGREEN='\033[1;32m'
@@ -12,6 +13,8 @@ BCYAN='\033[1;36m'
 BRED='\033[1;31m'
 NC='\033[0m'
 
+# $1: username
+# $2: ip
 function setupGitHubKey() {
     ssh $1@$2 << EOF
         echo $github_ssh_key >> ~/.ssh/known_hosts
@@ -20,16 +23,36 @@ EOF
 
 # $1: username
 # $2: ip
+function setupDataDir() {
+    ssh $1@$2 << EOF
+        sudo chown -R $1 /mydata
+EOF
+}
+
+# $1: username
+# $2: ip
 function installQEMU() {
     echo -e "${BCYAN}installing qemu for sekvm, could take a while...${NC}"
     ssh $1@$2 << EOF
-        sudo chown -R $1 /mydata
         cd /mydata
         git clone git@github.com:ntu-ssl/qemu.git	
         cd qemu
         git checkout $qemu_branch
 
         ./configure --target-list=aarch64-softmmu --disable-werror	
+        make -j8
+EOF
+}
+
+# $1: username
+# $2: ip
+function installGuestKernel() {
+    echo -e "${BCYAN}installing guest kernel, could take a while...${NC}"
+    ssh $1@$2 << EOF
+        cd /mydata
+        git clone git@github.com:ntu-ssl/linux-guest.git
+        cd linux-guest
+        git checkout $guest_branch
         make -j8
 EOF
 }
@@ -75,7 +98,9 @@ EOF
 # $2: ip
 function setup() {
     setupGitHubKey $1 $2
+    setupDataDir $1 $2
     installQEMU $1 $2
+    installGuestKernel $1 $2
     installTutorials $1 $2
     installSeKVM $1 $2
 }
