f50e9b21765b9f0804868cc03f8977307036a3fd
fjyang <fjyang@localhost>
2023-03-23 21:01:51 -0600

    update on eval

--- eval.sh
+++ eval.sh
@@ -4,23 +4,29 @@
 # -------------------- Setting --------------------- #
 
 ## VM scripts
-vm_src="qemu_scripts/blk.sh"
-vm_dst="qemu_scripts/resume-blk.sh"
+script_dir="qemu_scripts"
+vm_src="ab.sh"
+vm_dst="ab-res.sh"
+#vm_src="blk.sh"
+#vm_dst="resume-blk.sh"
 
 ## Network Settings
 src_ip="10.10.1.1"
 dst_ip="10.10.1.2"
+guest_ip="10.10.1.5"
 username="fjyang"
 src_monitor_port="1234"
-dst_monitor_port="1235"
+dst_monitor_port="1236"
 migration_port=8888
 
 ## Evaluation Settings
-ab="off"
-rounds=2
-expected_max_totaltime="20s"
-ParamsToSet[0]="multifd-channels"
-CapsToSet[0]="multifd"
+ab="on"
+rounds=10
+expected_max_totaltime="40s"
+#ParamsToSet[0]="multifd-channels"
+ParamsToSet[0]="downtime-limit"
+#CapsToSet[0]="multifd"
+CapsToSet[0]="postcopy-ram"
 
 # Note: field val can only be number
 FieldsToCollect[0]="downtime"
@@ -72,6 +78,7 @@ EOF
     err+=$(echo "$log" | grep "No such file or directory")
     if [[ -n "$err" ]]; then
         echo "Failed"
+	echo "$log" >&2
     fi
 }
 
@@ -190,8 +197,8 @@ for cap in "${CapsToSet[@]}"; do
 done
 
 echo -e "${BCYAN}uploading VM boot scripts${NC}"
-scp "$vm_src" $username@$src_ip:~/blk.sh
-scp "$vm_dst" $username@$dst_ip:~/resume-blk.sh
+scp "$script_dir/$vm_src" $username@$src_ip:~/"$vm_src"
+scp "$script_dir/$vm_dst" $username@$dst_ip:~/"$vm_dst"
 
 setupBridgeNetwork $src_ip
 setupBridgeNetwork $dst_ip
@@ -200,10 +207,14 @@ mkdir $output_dir 2>/dev/null
 
 for (( i = 0; i < $rounds; i++ )); do
 
+
+    # FIXME: hard coded path
+    sudo cp /proj/ntucsie-PG0/fjyang/cloud-hack-ab-bak.img /proj/ntucsie-PG0/fjyang/cloud-hack-ab.img
+
     # boot VM
     result=""
-    result=$(bootVM $src_ip blk.sh)
-    result+=$(bootVM $dst_ip resume-blk.sh)
+    result=$(bootVM $src_ip "$vm_src")
+    result+=$(bootVM $dst_ip "$vm_dst")
     if [[ -n "$result" ]]; then
         echo -e "${BRED}boot VM failed${NC}" >&2
         rebootM400 $src_ip
@@ -235,6 +246,7 @@ for (( i = 0; i < $rounds; i++ )); do
 
 
     if [[ $ab == "on" ]]; then 
+        ab_fn="$output_dir/ab_$i.txt"
         echo -e "${BCYAN}starting ab${NC}" >&2
         ab -c 100 -n 100000000000000000 -s 60 -g "$ab_fn" http://10.10.1.5/ &
         ab_pid=$!
@@ -243,6 +255,8 @@ for (( i = 0; i < $rounds; i++ )); do
 
     echo -e "${BCYAN}starting the migration${NC}" >&2
     ncat -w 5 -i 2 $src_ip $src_monitor_port <<< "$command_migrate" 2>/dev/null > /dev/null
+    # FIXME: hard coded postcopy start
+    ncat -w 5 -i 2 $src_ip $src_monitor_port <<< "postcopy_start" 2>/dev/null > /dev/null
     echo -e "${BCYAN}wait for the migration to complete${NC}" >&2
     sleep "$expected_max_totaltime"
 
@@ -250,7 +264,6 @@ for (( i = 0; i < $rounds; i++ )); do
     echo -e "${BCYAN}fetching migration results${NC}" >&2
     src_fn="$output_dir/src_$i.txt"
     dst_fn="$output_dir/dst_$i.txt"
-    ab_fn="$output_dir/ab_$i.txt"
     ncat -w 10 -i 10 $src_ip $src_monitor_port <<< "$command_info" 2> /dev/null | strings > $src_fn
     ncat -w 10 -i 10 $dst_ip $dst_monitor_port <<< "$command_info" 2> /dev/null | strings > $dst_fn
     dos2unix $src_fn
@@ -267,6 +280,16 @@ for (( i = 0; i < $rounds; i++ )); do
         sudo kill -SIGINT "$ab_pid"
         sleep 10s
     fi
+
+
+    if curl "$guest_ip" 2>&1 > /dev/null; then
+        echo -e "${BGREEN}dst alive${NC}" >&2
+    else
+        echo -e "${BRED}dst dead${NC}" >&2
+        result+="Failed"
+    fi
+
+
     result+=$(checkValidity $src_fn)
     result+=$(checkValidity $dst_fn)
     if [[ -n "$result" ]]; then
@@ -288,9 +311,15 @@ for (( i = 0; i < $rounds; i++ )); do
 
     echo -e "${BCYAN}cleaning up VMs${NC}" >&2
     ncat -w 5 -i 2 $src_ip $src_monitor_port <<< "$command_shutdown" 2> /dev/null > /dev/null
+    # use `halt -p` so that the image won't corrupt
+    tmp=$( { ssh root@$guest_ip << EOF
+    halt -p
+EOF
+    } 2>&1 )
+    sleep 10s
     ncat -w 5 -i 2 $dst_ip $dst_monitor_port <<< "$command_shutdown" 2> /dev/null > /dev/null
     echo -e "${BCYAN}wait for VMs to shutdown${NC}" >&2
-    sleep 40s
+    sleep 50s
 
 done
 
