422a8313bb31028729b1dbf592c80041b91d988e
fjyang <fjyang@localhost>
2023-04-24 00:58:47 -0600

    update batch.sh

--- batch.sh
+++ batch.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
-./eval.sh precopy 1000 1024 1 off off >> test_out
-./eval.sh multifd2 1000 1024 2 on off >> test_out
-./eval.sh multifd4 1000 1024 4 on off >> test_out
-./eval.sh multifd8 1000 1024 8 on off >> test_out
-./eval.sh postcopy 1000 1024 1 off on >> test_out
+./eval.sh precopy 1000 1024 1 0 off off off >> test_out
+./eval.sh compress 1000 1024 1 0 off off on >> test_out
+./eval.sh multifd2 1000 1024 2 0 on off off >> test_out
+./eval.sh multifd4 1000 1024 4 0 on off off >> test_out
+./eval.sh multifd8 1000 1024 8 0 on off off >> test_out
+./eval.sh postcopy 1000 1024 1 10737418240 off on off >> test_out
