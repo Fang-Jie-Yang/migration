4758f4f41d130f0a9ab23041da7153bd49b60354
fjyang <fjyang@localhost>
2023-08-15 08:08:29 -0600

    wip: reworking scripts

--- config.sh
+++ config.sh
@@ -3,26 +3,42 @@
 VM_DISK_IMAGE="/proj/ntucsie-PG0/fjyang/cloud-hack-ab-bak.img"
 NFS_PATH="/proj/ntucsie-PG0/fjyang/cloud-hack-ab.img"
 
-QEMU_PATH="/mydata/qemu"
-#VM_KERNEL="/mydata/some-tutorials/files/sekvm/Image.sekvm.guest"
-VM_KERNEL="/mydata/some-tutorials/files/Image.sekvm"
 SRC_IP="10.10.1.1"
 DST_IP="10.10.1.2"
+QEMU_PATH="/mydata/qemu"
+VM_KERNEL="/mydata/some-tutorials/files/Image.sekvm"
+#VM_KERNEL="/mydata/some-tutorials/files/sekvm/Image.sekvm.guest"
 MONITOR_PORT="1234"
+MIGRATION_PORT="8888"
 QEMU_CMD="$QEMU_PATH/aarch64-softmmu/qemu-system-aarch64 \
-		-enable-kvm \
-		-M virt \
-		-cpu host \
-		-smp 4 \
-		-m 256 \
-		-kernel $VM_KERNEL \
-		-netdev tap,id=net1,helper=$QEMU_PATH/qemu-bridge-helper,vhost=on \
-		-device virtio-net-pci,netdev=net1,mac=de:ad:be:ef:f6:5f \
-		-drive if=none,file=$NFS_PATH,id=vda,cache=none,format=raw \
-		-device virtio-blk-pci,drive=vda \
-		-append \"console=ttyAMA0 root=/dev/vda rw earlycon=pl011,0x09000000\" \
-		-display none \
-		-daemonize"
+    -enable-kvm \
+    -M virt \
+    -cpu host \
+    -smp 4 \
+    -m 1024 \
+    -kernel $VM_KERNEL \
+    -netdev tap,id=net1,helper=$QEMU_PATH/qemu-bridge-helper,vhost=on \
+    -device virtio-net-pci,netdev=net1,mac=de:ad:be:ef:f6:5f \
+    -drive if=none,file=$NFS_PATH,id=vda,cache=none,format=raw \
+    -device virtio-blk-pci,drive=vda \
+    -append 'console=ttyAMA0 root=/dev/vda rw earlycon=pl011,0x09000000' \
+    -display none \
+    -daemonize"
+SRC_QEMU_CMD="$QEMU_CMD \
+    -monitor telnet:$SRC_IP:$MONITOR_PORT,server,nowait"
+DST_QEMU_CMD="$QEMU_CMD \
+    -monitor telnet:$DST_IP:$MONITOR_PORT,server,nowait \
+    -incoming tcp:0:$MIGRATION_PORT"
 
+GUEST_IP="10.10.1.5"
+
+MIGRATION_PROPERTIES=(
+    "migrate_set_parameter downtime-limit 300"
+    "migrate_set_parameter max-bandwidth 102400"
+    "migrate_set_parameter multifd-channels 4"
+    "migrate_set_parameter max-postcopy-bandwidth 107374182400"
+    "migrate_set_capability multifd on"
+    "migrate_set_capability postcopy-ram off"
+)
 
 
--- script.sh
+++ script.sh
@@ -30,10 +30,8 @@ function setup_vm_env() {
 function boot_vm() {
     log_msg "Booting VM on $1"
 
-    cmd="$QEMU_CMD -monitor telnet:$1:$MONITOR_PORT,server,nowait"
-
     ret=$( { ssh $(whoami)@$1 << EOF
-    sudo /srv/vm/net.sh; sudo nohup $cmd
+    sudo /srv/vm/net.sh; sudo nohup $2
 EOF
     } 2>&1 > /dev/null)
 
@@ -64,20 +62,93 @@ EOF
     return 0
 }
 
+function check_guest_status() {
+    log_msg "Checking vm's status"
+    if ! ping -c 3 "$GUEST_IP" >&2 ; then
+        return $RETRY
+    fi
+    return 0
+}
+
+# qemu_monitor_send(ip, port, cmd)
+function qemu_monitor_send() {
+    if ! echo "$3" | ncat -w 1 $1 $2; then
+        return $RETRY
+    fi
+    return 0
+}
+
+function start_migration() {
+    for cmd in "${MIGRATION_PROPERTIES[@]}"; do
+        if ! qemu_monitor_send $SRC_IP $MONITOR_PORT "$cmd"; then
+            return $RETRY
+        fi
+    done
+    cmd="migrate -d tcp:$DST_IP:$MIGRATION_PORT"
+    if ! qemu_monitor_send $SRC_IP $MONITOR_PORT "$cmd"; then
+        return $RETRY
+    fi
+    return 0
+}
+
+function force_clean_up() {
+    cmd="sudo pkill -9 qemu"
+    echo "$cmd" | ssh -q $(whoami)@$1 >/dev/null
+    if [[ $? -eq 255 ]]; then
+        err_msg "Failed to force clean up"
+        return $ABORT
+    fi
+    return 0
+}
+
+function clean_up() {
+    if ! qemu_monitor_send $1 $MONITOR_PORT "quit"; then
+        err_msg "Failed to clean up"
+        if ! force_clean_up $1; then
+            return $ABORT
+        fi
+    fi
+    return 0
+}
+
 function do_migration_eval() {
 
     if ! setup_vm_env ; then
+        ret=$?
         err_msg "Failed to setup environment"
-        return $RETRY;
+        return $ret;
     fi
 
-    if ! boot_vm "$SRC_IP" ; then
-	ret=$?
+    if ! boot_vm "$SRC_IP" "$SRC_QEMU_CMD"; then
+        ret=$?
         err_msg "Failed to boot at src"
+        return $ret
+    fi
+
+    if ! boot_vm "$DST_IP" "$DST_QEMU_CMD"; then
+        ret=$?
+        err_msg "Failed to boot at dst"
+        return $ret
+    fi
+
+    sleep 10s
+
+    if ! check_guest_status; then
+        ret=$?
+        err_msg "VM status unknown"
+        return $ret
+    fi
+        
+    if ! start_migration; then
+        ret=$?
+        err_msg "Failed to start migration"
         return $ret
     fi
 }
 
 
+
 source config.sh
 do_migration_eval
+clean_up $SRC_IP
+clean_up $DST_IP
